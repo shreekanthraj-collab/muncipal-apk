37546375bbb5f8486475d72369dba4cd5b23085c
Shreekanth <shreekanth.raj@gmail.com>
2026-09-14 14:36:23 +0530

    feat(admin): start frozen admin application shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,2 @@
-import 'package:flutter/material.dart';
-import 'screens/valve_detail_screen.dart';
-void main() {
-	runApp(const OrbiValveApp());
-}
-
-class OrbiValveApp extends StatelessWidget {
-	const OrbiValveApp({super.key});
-
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			debugShowCheckedModeBanner: false,
-			title: 'ORBI Valve',
-			theme: ThemeData(
-				useMaterial3: true,
-				colorSchemeSeed: Colors.blue,
-			),
-			home: const ValveDetailScreen(),
-		);
-	}
-}
+import 'admin_app.dart';
+void main() => runApp(const AdminValveApp());
