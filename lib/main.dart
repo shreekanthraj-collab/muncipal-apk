bca9bd323fc032fad4854969fe691189024c722e
Shreekanth <shreekanth.raj@gmail.com>
2026-09-17 15:13:33 +0530

    Fix app-wide portrait orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(const [
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const OrbiValveApp());
 }
 
