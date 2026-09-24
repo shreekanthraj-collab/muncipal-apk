ae903836aa102e4ceb81d146de9b422ff459f0d1
Shreekanth <shreekanth.raj@gmail.com>
2026-09-24 20:10:56 +0530

    Refresh map when valve fault state changes

--- lib/services/valve_fault_storage.dart
+++ lib/services/valve_fault_storage.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ValveFaultStorage {
   static const _prefix = 'oc_fault_';
 
+  // Map screen listens to this so pin colors update immediately.
+  static final ValueNotifier<int> changes = ValueNotifier<int>(0);
+
   static Future<void> save(String valveId, bool ocFault) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('$_prefix$valveId', ocFault);
+    changes.value++;
   }
 
   static Future<bool> load(String valveId) async {
